c0c9674b062143428edd80fbbe495e9ceb97c36e
Jose Miguel Paniagua Tinajero <ttn.developer@gmail.com>
2024-06-10 18:43:59 -0600

    Dashboard design.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  breakpoint: number = 3;
+
+  ngOnInit(): void {
+    const width = window.innerWidth;
+    //this.breakpoint = (window.innerWidth <= 600) ? 1 : 3;
+    switch (true) {
+      case width <= 650:
+        this.breakpoint = 1;
+        break;
+      case width > 650 && width <= 800:
+        this.breakpoint = 2;
+        break;
+      case width > 800 && width <= 1024:
+        this.breakpoint = 3;
+        break;
+      default:
+        this.breakpoint = 3;
+        break;
+    }
+  }
+
+  onResize(event: any): void {
+    //this.breakpoint = (event.target.innerWidth <= 600) ? 1 : 3;
+    const width = event.target.innerWidth;
+    switch (true) {
+      case width <= 600:
+        this.breakpoint = 1;
+        break;
+      case width > 600 && width <= 800:
+        this.breakpoint = 2;
+        break;
+      case width > 800 && width <= 1200:
+        this.breakpoint = 2;
+        break;
+      default:
+        this.breakpoint = 3;
+        break;
+    }
+  }
 
 }
